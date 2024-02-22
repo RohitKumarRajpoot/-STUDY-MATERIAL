50fcd683988c5ba820b142cd91d4a26e66839b9e
Rohit Kumar Rajpoot <160825935+RohitKumarRajpoot@users.noreply.github.com>
2024-02-22 12:24:54 +0530

    Add files via upload

---
+++ RGPV.js
@@ -0,0 +1,12 @@
+let CSE = [
+    {songName: "ek lamha tere bin jiya h channa we", filePath: "songs/1.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Dill galti kar beth h", filePath: "songs/2.mp3", coverPath: "covers/2.jpeg"},
+    {songName: " Hamne mohobat ki pathan moves song", filePath: "songs/3.mp3", coverPath: "covers/1.jpg"},
+    {songName: " mere jindgi se jane ka kya loge tum ", filePath: "songs/4.mp3", coverPath: "covers/1.jpg"},
+    {songName: " nena jo shajhe khowab dekhte the ", filePath: "songs/5.mp3", coverPath: "covers/1.jpg"},
+    {songName: " panipat me fear tera guje pura ambadha re", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: " ye pahli baar ka milna bhi", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: " kya tumhe pata h H gulsan dilwaar aane wale h", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/1.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/1.jpg"},
+]
